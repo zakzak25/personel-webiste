aeb35484e3b2a5f04a0f7d295e2a8b7223fbdbc9
zakzak25 <zbousba4@gmail.com>
2024-04-18 02:34:21 +0200

    the http request work perfect , and the bycript also work good

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = express.Router();
 require('dotenv').config();
 const dbconnection = require('../database/db');
+const bcrypt = require('bcrypt');
 
 
 
@@ -12,21 +13,35 @@ routes.get('/' , (req , res) => {
 
 //registration page 
 routes.get('/registration' , (req , res) => {
-    res.render('signup');
+    res.render('signup' , {title : 'Sign up page '});
 });
 
 
 
 //send data from html page to the back end
-routes.post('/newUser' , (req , res)=> {
-    const {username , email , password} = req.body
-    dbconnection.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username , email , password] , (err , result) => {
-        if (err) {
-            console.log("Err message : " + err.sqlMessage);
-            console.log("sql message : " + err.sql);
-        };
-        res.send('<p>new user is succefully submited..</p>')
-    });
+routes.post('/newUser' , async (req , res)=> {
+    try {
+        const {username, email ,password} = req.body;
+        dbconnection.query('SELECT email FROM users WHERE email = ?;', [email] , async (err , result) => {
+           if (result.length === 0) {
+            let hachedPassword = await bcrypt.hash(password , 8);
+                dbconnection.query('INSERT INTO users (username, email, password) VALUES (?, ?, ?)', [username , email , hachedPassword] , (err) => {
+                    if (err) {
+                        console.log("Err message : " + err.sqlMessage);
+                        console.log("sql message : " + err.sql);
+                    }else {
+                        res.send('<p>new user is succefully submited..</p>');
+                    }
+                });
+           }else {
+            res.render('signup' , {exist : true, message : 'email already exist, please <a href="/sign in" here.!'});
+           }
+        });
+    }
+    catch(err) {
+        console.log('Error:' , err);
+        res.status(500).send('An unexprected error occured.');
+    };
 });
 
 
